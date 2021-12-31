898ea4eda4f58bdf570b8267f17722bbbae9d50d
SatoruItaya <str.itaya@gmail.com>
2021-12-31 16:34:10 +0900

    update version

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "= 1.0.11"
+  required_version = "= 1.1.2"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "=3.68.0"
+      version = "=3.70.0"
     }
   }
 }
