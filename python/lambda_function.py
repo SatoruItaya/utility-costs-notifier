2b7fbf49b61509a8bbbf638692b4b5e436c7de5c
SatoruItaya <str.itaya@gmail.com>
2022-01-29 12:20:01 +0900

    add page link transition

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -9,10 +9,10 @@ def lambda_handler():
 
     driver = webdriver.Chrome('./chromedriver')
 
+    # login page
     driver.get('https://members.tokyo-gas.co.jp/')
-
-    login_button = driver.find_element_by_link_text('ログイン')
-    login_button.click()
+    login_page_link = driver.find_element_by_class_name('mtg-button-cta').get_attribute('href')
+    driver.get(login_page_link)
 
     login_id = driver.find_element_by_xpath('//input[@name="loginId"]')
     login_id.send_keys(os.environ['GASS_ID'])
@@ -20,14 +20,23 @@ def lambda_handler():
     password = driver.find_element_by_xpath('//input[@name="password"]')
     password.send_keys(os.environ['GASS_PASSWORD'])
 
+    # top page
     submit_btn = driver.find_element_by_id('submit-btn')
     submit_btn.click()
 
-    sleep(10)
-    # search_bar = driver.find_element_by_name("q")
-    # search_bar.send_keys("python")
+    sleep(5)
 
-    # search_bar.submit()
+    # total page
+    total_page_link = driver.find_element_by_class_name('mtg-content-number-link').get_attribute('href')
+    driver.get(total_page_link)
+
+    sleep(5)
+
+    # detail page
+    detail_page_link = driver.find_element_by_class_name('mtg-button-link').get_attribute('href')
+    driver.get(detail_page_link)
+
+    sleep(5)
 
 
 if __name__ == "__main__":
