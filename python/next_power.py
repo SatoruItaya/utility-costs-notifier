898a86cd6d1d06d2473618609c0b8727ebfeb8a1
SatoruItaya <str.itaya@gmail.com>
2022-03-20 18:26:44 +0900

    implement usage and billing amount

--- python/next_power.py
+++ python/next_power.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from time import sleep
 
+import datetime
+
 
 def get_electricity_cost(driver, id, password):
 
@@ -24,8 +26,30 @@ def get_electricity_cost(driver, id, password):
 
     sleep(5)
 
+    now = datetime.datetime.now()
     driver.switch_to.frame(driver.find_element(By.TAG_NAME, 'iframe'))
-    value = driver.find_element(By.XPATH, '//*[@id="Tbl_MonthTable"]/tbody/tr[3]/td[3]/div').text
-    print(value)
+
+    if now.month == 1:
+        previous_year_btn = driver.find_element(By.XPATH, '//*[@id="lbPre"]')
+        previous_year_btn.click()
+        sleep(5)
+        usage_amount = driver.find_element(By.XPATH, '//*[@id="Tbl_MonthTable"]/tbody/tr[7]/td[5]/div').text
+        billing_amount = driver.find_element(By.XPATH, '//*[@id="Tbl_MonthTable"]/tbody/tr[7]/td[6]/div').text
+    else:
+        previous_month = now.month - 1
+        if previous_month <= 6:
+            usage_amount = driver.find_element(By.XPATH, '//*[@id="Tbl_MonthTable"]/tbody/tr[' + str(previous_month + 1) + ']/td[2]/div').text
+            billing_amount = driver.find_element(By.XPATH, '//*[@id="Tbl_MonthTable"]/tbody/tr[' + str(previous_month + 1) + ']/td[3]/div').text
+        else:
+            usage_amount = driver.find_element(By.XPATH, '//*[@id="Tbl_MonthTable"]/tbody/tr[' + str(previous_month - 6 + 1) + ']/td[5]/div').text
+            billing_amount = driver.find_element(By.XPATH, '//*[@id="Tbl_MonthTable"]/tbody/tr[' + str(previous_month - 6 + 1) + ']/td[6]/div').text
+
+    print(usage_amount)
+    print(billing_amount)
 
     sleep(5)
+
+    # TODO
+    # componentを関数に
+    # 前年遷移
+    # usage_amountとbilling_amountを取得
